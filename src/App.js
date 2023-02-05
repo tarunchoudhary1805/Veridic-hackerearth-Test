46ebd7d82e211f5eed95946e6c8e0da67a46962d
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-02-05 18:24:37 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+import parse from "html-react-parser";
 
 function App() {
+  const [state, setState] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const data = await axios.get(
+        `https://techcrunch.com/wp-json/wp/v2/posts?per_page=20&context=embed`
+      );
+
+      setState(data.data);
+      setLoading(false);
+    })();
+  }, []);
+
+  console.log(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="heading">New Articles</h2>
+      <div className="App">
+        {loading && <span class="loader"></span>}
+        {!loading &&
+          (state?.length > 0 ? (
+            state?.map((blog) => (
+              <a className="link" href={blog.canonical_url} target="_blank">
+                <div class="card">
+                  <div className="flex flex-direction justify-content-start align-items-center">
+                    <div>
+                      <img
+                        src={blog.jetpack_featured_media_url}
+                        className="image"
+                        alt="img"
+                      />
+                    </div>
+                    <div className="flex flex-direction-column justify-content-start">
+                      <div class="text">
+                        <h2>{parse(blog.title.rendered)}</h2>
+                      </div>
+                      <div className="text">
+                        <p>{parse(blog.excerpt.rendered)}</p>
+                        <br />
+                        <p>{new Date(blog.date).toDateString()}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            ))
+          ) : (
+            <p>No Articles found</p>
+          ))}
+      </div>
     </div>
   );
 }
